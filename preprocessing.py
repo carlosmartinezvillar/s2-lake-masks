6383758a7ba471e119331401badf332d5303965c
Carlos Martinez-Villar <cimcrd@umsystem.edu>
2024-01-06 22:32:10 -0600

    added checkerboard printing/check function to see how chips are coming out of label

--- preprocessing.py
+++ preprocessing.py
@@ -26,6 +26,7 @@ ns = {
 
 plt.style.use('fast')
 DATA_DIR  = './dat/'  #<---- change this to argparse? maybe env
+LABEL_DIR = 'dynamicworld'
 TILE_SIZE = 256
 
 ################################################################################
@@ -195,7 +196,7 @@ def plot_label_multiclass(path,dw_rdr,borders,window=None):
 	return
 
 
-def plot_label_multiclass_windowed(path,dw_rdr,borders,windows):
+def plot_label_multiclass_windows(path,dw_rdr,borders,windows):
 	DW_PALETTE_10 = ['000000','419bdf','397d49','88b053','7a87c6','e49635', 
 	    'dfc35a','c4281b','a59b8f','b39fe1'];
 
@@ -224,20 +225,8 @@ def plot_label_multiclass_windowed(path,dw_rdr,borders,windows):
 
 	dst.close()
 
-
-def print_checkerboard(safe_dir,windows,quant_val,offset_vals):
-	#yellow #FFFF00
-	yellow_line      = np.ones((3,TILE_SIZE))
-	yellow_line[0:2] = 65535
-	yellow_line[2]   = 0
-	red_line         = np.ones((3,TILE_SIZE))
-	red_line[0] = 65535
-
-
-	return
-
 #TODO
-def save_tci_full_windowed(dst_path,bands,borders,windows]):
+def plot_tci_windows(dst_path,bands,borders,windows):
 
 	#need to know size of output
 	height = borders['bottom'] - borders['top'] + 1
@@ -248,7 +237,7 @@ def save_tci_full_windowed(dst_path,bands,borders,windows]):
 	return True
 
 #TODO
-def save_tci_full(path, bands, quant_val, offsets):
+def plot_tci(path, bands, quant_val, offsets):
 	"""
 	Writing is in the order B,G,R.
 	"""
@@ -281,6 +270,128 @@ def save_tci_full(path, bands, quant_val, offsets):
 	print("TCI written to %s" % path)
 
 	return True
+
+
+def plot_checkerboard(path,dw_rdr,borders,windows):
+	"""
+	Plot whole original label image with overlaying squares corresponding to
+	the blocks/windows used as selection for the chips.
+	"""
+	DW_PALETTE_10 = ['000000','419bdf','397d49','88b053','7a87c6','e49635', 
+	    'dfc35a','c4281b','a59b8f','b39fe1'];
+
+	#Get lines -- red FF0000, yellow FFFF00
+	yellow_line      = np.ones((3,TILE_SIZE))
+	yellow_line[0:2] = 65535
+	yellow_line[2]   = 0
+	red_line         = np.ones((3,TILE_SIZE))
+	red_line[0]      = 65535
+	red_line[1:2]    = 0
+
+	height = borders['bottom'] - borders['top'] + 1
+	width  = borders['right'] - borders['left'] + 1
+
+	#WRITER
+	kwargs = dw_rdr.meta.copy()
+	kwargs.update({'count':3,'compress':'lzw','height':height,'width':width})
+	out_ptr = rio.open(path,'w',**kwargs)
+
+	correct_count = 0
+
+	#USE WINDOWS TO PLOT SQUARES
+	for _,w in windows:
+		arr = dw_rdr.read(1,window=w)
+		arr3 = np.stack([arr,np.zeros_like(arr),np.zeros_like(arr)],axis=0)
+		for c in range(10):
+			r = int(DW_PALETTE_10[c][0:2],16)
+			g = int(DW_PALETTE_10[c][2:4],16)
+			b = int(DW_PALETTE_10[c][4:6],16)
+			arr3[:,arr==c] = [[r],[g],[b]]	
+
+		#LINES
+		if (arr==0).sum() > 0:
+			arr3[:,0,:]  = red_line
+			arr3[:,-1,:] = red_line
+			arr3[:,:,0]  = red_line
+			arr3[:,:,-1] = red_line
+			arr3[:,1,:]  = red_line
+			arr3[:,-2,:] = red_line
+			arr3[:,:,1]  = red_line
+			arr3[:,:,-2] = red_line
+		else:
+			n_water = (arr==1).sum()
+			if n_water > 4096  and n_water < TILE_SIZE*TILE_SIZE:
+				arr3[:,0,:]  = yellow_line
+				arr3[:,-1,:] = yellow_line
+				arr3[:,:,0]  = yellow_line
+				arr3[:,:,-1] = yellow_line
+				arr3[:,1,:]  = yellow_line
+				arr3[:,-2,:] = yellow_line
+				arr3[:,:,1]  = yellow_line
+				arr3[:,:,-2] = yellow_line
+
+				correct_count += 1
+
+			else:
+				arr3[:,0,:]  = red_line
+				arr3[:,-1,:] = red_line
+				arr3[:,:,0]  = red_line
+				arr3[:,:,-1] = red_line
+				arr3[:,1,:]  = red_line
+				arr3[:,-2,:] = red_line
+				arr3[:,:,1]  = red_line
+				arr3[:,:,-2] = red_line
+		#WRITE
+		w2 = Window(w.col_off-borders['left'],w.row_off-borders['top'],TILE_SIZE,TILE_SIZE)
+		out_ptr.write(arr3,window=w2,indexes=[1,2,3])
+
+	#PLOT REMAINING IMAGE BEYOND WINDOWS
+	block_rows = height // TILE_SIZE
+	block_cols = width // TILE_SIZE
+	remainder_rows = height % TILE_SIZE
+	remainder_cols = width % TILE_SIZE
+
+	#left edge in the bottom
+	w1 = Window(
+		col_off=borders['left'],
+		row_off=borders['top']+block_rows*TILE_SIZE,
+		width=block_cols*TILE_SIZE,
+		height=remainder_rows) #reader,starts at border['top'],borders['left']
+
+	w2 = Window(
+		col_off=0,
+		row_off=block_rows*TILE_SIZE,
+		width=block_cols*TILE_SIZE,
+		height=remainder_rows) #writer,starts at 0,0
+
+	arr = dw_rdr.read(1,window=w1)
+	arr3 = np.stack([arr,np.zeros_like(arr),np.zeros_like(arr)],axis=0)
+	for c in range(10):
+		r = int(DW_PALETTE_10[c][0:2],16)
+		g = int(DW_PALETTE_10[c][2:4],16)
+		b = int(DW_PALETTE_10[c][4:6],16)
+		arr3[:,arr==c] = [[r],[g],[b]]
+	out_ptr.write(arr3,window=w2,indexes=[1,2,3])
+
+
+
+	#leftover edge in the right
+	w1 = Window(
+		col_off=borders['left']+block_cols*TILE_SIZE,
+		row_off=borders['top'],
+		width = remainder_cols,
+		height = height)
+
+	w2 = Window(
+		col_off=block_cols*TILE_SIZE,
+		row_off=0,
+		width=remainder_cols,
+		height=height) #writer, starts at 0,0
+
+
+	print("Nr of good blocks: %i" % correct_count)
+
+	return
 
 #TODO
 def save_tci_window(path,bands,quant_val,offsets,window):
@@ -631,6 +742,20 @@ def process_window(w: Window, ):
 
 #TODO
 def process_product(id: str):
+	#1.ID -> BAND PATHS, XML
+
+	#2.XML -> DW PATH, OFFSETS, QUANTIFICATION VALUE
+
+	#3.BAND PATHS, DW PATH -> DatasetReader x 5
+
+	#4.DW READER -> BOUNDARIES DW
+
+	#5.BOUNDARIES DW, BAND READER x 4 -> BOUNDARIES S2
+
+	#6.BOUNDARIES S2, BOUNDARIES DW -> WINDOWS DW, WINDOWS S2 
+
+	#7.ITERATE THROUGH WINDOWS
+
 	pass
 
 
@@ -653,7 +778,7 @@ if __name__ == '__main__':
 
 	#GET FILE PATHS
 	gee_id = get_gee_id(safe_dir,datastrip)
-	label_path = DATA_DIR + '/'.join(["dynamicworld",gee_id]) + '.tif'
+	label_path = DATA_DIR + '/'.join([LABEL_DIR,gee_id]) + '.tif'
 	band_fname = get_band_filenames(safe_dir,['B02','B03','B04','B08'])
 	band_paths = [DATA_DIR + safe_dir + '/' + _  for _ in band_fname]
 
@@ -670,6 +795,8 @@ if __name__ == '__main__':
 	#WINDOWS TO READ
 	input_windows = get_windows(input_borders)
 	label_windows = get_windows(label_borders)
+
+	plot_checkerboard('test_checkerboard.tif',label,label_borders,label_windows)
 
 	# save_tci_full('./test_tci.tif',[band2,band3,band4],quant_val,offset_vals[0:3])
 	# print_checkerboard(safe_dir,input_windows,quant_val,offset_vals)
